d67930107e7a2eb3d3d4479582f6fab628b6ce81
levimatan <levimatan@gmail.com>
2016-03-24 14:57:12 +0200

    change to check if null in insert sql

--- service.php
+++ service.php
@@ -285,7 +285,7 @@ if( strcasecmp($_GET['method'],'hello') == 0){
 
 	$retval = mysql_query( $sql, $conn );
     
-   	if(isset($retval)) {
+   	if(is_null($retval)) {
       //die('Could not enter data: ' . mysql_error());
    		$response['data'] = "error, email already exists. ".$retval;   		
    	} else {
