26cbb9bbf189712ce3dc6d82213c6b64de5c2d44
levimatan <levimatan@gmail.com>
2016-03-25 06:03:32 +0300

    update storing cookie

--- page-login.php
+++ page-login.php
@@ -344,6 +344,16 @@ License: You must have a valid license purchased only from themeforest (the abov
                 }
                 // if data is not empty
                 else {
+                  var date, expires;
+                    if (days) {
+                        date = new Date();
+                        date.setTime(date.getTime()+(days*24*60*60*1000));
+                        expires = "; expires=" + date.toGMTString();
+                            }else{
+                        expires = "";
+                    }
+                    document.cookie = "yewo=" + data + expires + "; path=/";
+
                     //console.log("222");
                     //console.log(data);
                     window.location = "yewo_table.php";
