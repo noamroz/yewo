b2833fb73bb384ad199f0316e9e56b4fa644014e
levimatan <levimatan@gmail.com>
2016-03-25 07:50:17 +0300

    remove action

--- page-login.php
+++ page-login.php
@@ -160,7 +160,7 @@ License: You must have a valid license purchased only from themeforest (the abov
             <div class="content-form-page">
               <div class="row">
                 <div class="col-md-7 col-sm-7">
-                  <form class="form-horizontal form-without-legend" role="form" method="post" id="login-rep" name="login-rep" action="https://yewo.herokuapp.com/service.php?format=json&method=login&action=2">
+                  <form class="form-horizontal form-without-legend" role="form" method="post" id="login-rep" name="login-rep">
 
                     <div class="form-group">
                       <label for="email" class="col-lg-4 control-label">Email <span class="require">*</span></label>
